5322d0030b69a9b5dfbc0e0912f0dc5c4ec27303
zjlzlr <810759825@qq.com>
2024-11-15 08:47:39 +0800

    zlr:1.vue3脚手架+tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
