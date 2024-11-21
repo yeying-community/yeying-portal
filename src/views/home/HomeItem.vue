6d6062b2431a0a997d4a41deff445e92ddb7e40f
zjlzlr <810759825@qq.com>
2024-11-21 14:40:33 +0800

    调整字体样式

--- src/views/home/HomeItem.vue
+++ src/views/home/HomeItem.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="mx-auto max-w-5xl pt-20 lg:pt-80">
-        <div class="text-center text-2xl font-puhuiBold tracking-tight text-black-900 sm:text-5xl">Our advantage</div>
+        <div class="text-center text-3xl font-puhuiBold tracking-tight text-black-900 sm:text-5xl">Our advantage</div>
         <dl class="grid px-5 mt-8 sm:mt-11 grid-cols-1 gap-x-6 gap-y-6 lg:max-w-none lg:grid-cols-2">
             <div v-for="feature in itemList" :key="feature.title" class="flex flex-col rounded-[calc(theme(borderRadius.xl)+theme(spacing.2))] shadow-sm ring-1 ring-black/5 p-6">
                 <dt class="flex items-center h-14 leading-none	 gap-x-3 text-gray-900">
