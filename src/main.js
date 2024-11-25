f941879df14e170586980064721df07f3c04433b
kobor <kobofare@gmail.com>
2024-11-25 13:39:54 +0800

    update package version

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import '@/assets/style.css'
 
+import {t, setLanguage} from '@yeying-community/yeying-i18n'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
@@ -8,6 +9,11 @@ import router from '@/router'
 import lang from '@/lang'
 
 const app = createApp(App)
+await setLanguage('zh-CN')
+console.log(t('common.confirm'))
+
+await setLanguage('en-US')
+console.log(t('common.confirm'))
 
 app.use(createPinia())
 app.use(router)
