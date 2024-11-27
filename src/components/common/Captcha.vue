afbb5961336b58661377dd687fadc99d79f3e83f
zjlzlr <810759825@qq.com>
2024-11-27 08:12:59 +0800

    zlr:验证码

--- src/components/common/Captcha.vue
+++ src/components/common/Captcha.vue
@@ -1,6 +1,36 @@
 <template>
-    <div>==
-    </div>
+  <div class="login-container">
+      <!-- 后端验证距离滑块 -->
+      <SlideSplit
+        ref="slideRef"
+        w="200"
+        h="120"
+        st="3"
+        :bigImg="imgAjaxInfo.bigImg"
+        :smImg="imgAjaxInfo.smImg"
+        @sliderJudge="onJudge"
+      />
+    <!-- </a-modal> -->
+  </div>
 </template>
+
 <script setup>
+import { reactive, ref, toRefs } from "vue";
+import SlideSplit from "./SliderCaptcha.vue";
+import { bgBase64,blockBase64 } from './test';
+const slideRef = ref()
+const imgAjaxInfo = ref({
+  bigImg: bgBase64,
+  smImg: blockBase64
+})
+const onJudge = (pos1,pos2) => {
+  slideRef.value.onFail()
+  // slideRef.value.reset()
+  console.log("judge", pos1,pos2)
+}
+
 </script>
+
+<style scoped>
+
+</style>
