4323a4bf485cce5e56a57047c7f313e6b33dc477
zjlzlr <810759825@qq.com>
2025-01-04 08:58:08 +0800

    接口接入

--- src/plugins/account.js
+++ src/plugins/account.js
@@ -4,22 +4,30 @@ class $account {
     this.manager = new AccountManager()
     console.log("account:",this.manager)
   }
+  getAccout(){
+    return this.manager
+  }
+  getBlockAddress() {
+    const info = this.manager.getActiveAccount()
+    return this.manager.getBlockAddress(info.did)
+  }
   // 根据did判断是否登录
-  isLogin() {
-    const did = localStorage.getItem("did");
-    if(did){
-      const check = this.manager.isLogin(did)
-      return !!check
+  async isLogin() {
+    const info = this.manager.getActiveAccount()
+
+    const {did,name} = info || {}
+    if(name=="Guest"){
+      await this.manager.login(did)
     }
-    return false
+    const check = this.manager.isLogin(did)
+    return !!check
   }
   // 创建游客身份
   async createGuest() {
     const info = await this.manager.createGuest()
     // 登录
     const did = info.metadata.did
-    this.manager.login(did)
-    localStorage.setItem("did", did)
+    // await this.manager.login(did)
     return info
   }
 }
