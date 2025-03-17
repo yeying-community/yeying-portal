8bfbc5f420d3513dafe50f49af874cda71e6fa2c
zjlzlr <810759825@qq.com>
2025-03-17 20:55:58 +0800

    issue问题修改

--- src/views/home/HomeDesc.vue
+++ src/views/home/HomeDesc.vue
@@ -1,14 +1,42 @@
 <script setup>
 import Contact from '@/components/contact/Modal.vue'
 import {getCurrentInstance, ref} from 'vue'
+import $account from '@/plugins/account.js'
+import { useRouter } from 'vue-router'
 
 const {proxy} = getCurrentInstance()
 const {$t} = proxy
 const open = ref(false)
 const RefContact = ref()
-const openContact = () => {
-  open.value = true
-  RefContact.value.openModal()
+const router = useRouter();
+
+const changeLogin = async () => {
+  const info = await $account.getActiveIdentity()
+  // const did = info && info.metadata && info.metadata.did
+  const did = $account.getActiveDid()
+  let isLogin = false
+  if(did){
+    try{
+      // await $account.login(did)
+      isLogin = $account.isLogin(did)
+    }catch(e){
+      console.error('login failed:',e)
+    }
+  }
+  if(isLogin){
+    return true
+  }else{
+    router.push("/toLogin")
+    // RefLogin.value.openModal()
+  }
+  return false
+}
+const openContact = async () => {
+  const isLogin = await changeLogin()
+  if(!isLogin){
+    return
+  }
+  // 跳转到应用商店页
 }
 const close = () => {
   open.value = false
@@ -29,13 +57,13 @@ const close = () => {
         open infrastructure, and provide the ability to DIY various Web3 applications.
       </div>
       <div class="mt-14 flex items-center justify-center gap-x-6">
-        <button
+        <!-- <button
             type="button"
             @click="openContact"
             class="blue-color rounded-full font-puhuiRegular bg-white px-3 sm:px-8 py-2.5 text-base sm:text-xl shadow-sm ring-1 ring-inset ring-blue-300 hover:bg-blue-50">
           {{ $t("common.contactUs") }}
-        </button>
-        <button type="button"
+        </button> -->
+        <button type="button" @click="openContact"
                 class="mx-2.5 font-puhuiRegular rounded-full bg-blue-600 px-3 sm:px-8 py-2.5 py-2.5 text-base sm:text-xl text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">
           {{ $t("portal.freeStart") }}
         </button>
