96a1ff7e0966eba264ad8fd75cd6e8cb95349f8b
zjlzlr <810759825@qq.com>
2025-06-14 21:17:53 +0800

    test

--- src/main.js
+++ src/main.js
@@ -6,8 +6,8 @@ import {createPinia} from 'pinia'
 import App from '@/App.vue'
 import { routes } from '@/router'
 import { createRouter, createWebHistory } from 'vue-router'
-import {createWallet,sdkRoutes} from '@yeying-community/yeying-wallet';
-import { initializeProviders } from '@/plugins/account'
+// import {createWallet,sdkRoutes} from '@yeying-community/yeying-wallet';
+import $account from '@/plugins/account2'
 
 const app = createApp(App)
 
@@ -23,16 +23,18 @@ const router = createRouter({
 // 需要登录就需要添加这个路由守卫,如果是不是公开路由，并且没有登录，跳转到登录页面
 // sdkSetupRouter(router) // sdk路由守卫
 // setupRouter(router) // 项目路由守卫
+const isLogin = $account.isLogin()
+console.log('isLogin',isLogin)
 app.use(router)
 // sdk初始化调用
-createWallet(router,initializeProviders,{
-  // 登录成功回调
-  onLoginSuccess: (account) => {
-    console.log('登录成功',account);
-  },
-  // 创建身份成功回调
-  onCreateSuccess: () => {
-    console.log('注册成功');
-  }
-})
+// createWallet(router,initializeProviders,{
+//   // 登录成功回调
+//   onLoginSuccess: (account) => {
+//     console.log('登录成功',account);
+//   },
+//   // 创建身份成功回调
+//   onCreateSuccess: () => {
+//     console.log('注册成功');
+//   }
+// })
 app.mount('#app')
