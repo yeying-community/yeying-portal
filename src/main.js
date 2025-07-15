83a9a9f44833de283cf11d0dde3797a6b9f1a112
tiantian <1489423489@qq.com>
2025-07-15 22:45:47 +0800

    fix: 格式化

--- src/main.js
+++ src/main.js
@@ -1,22 +1,22 @@
-import "@/assets/style.css";
-import { t } from "@yeying-community/yeying-i18n";
-import "@yeying-community/yeying-wallet/dist/yeying-wallet.css";
-import { createApp } from "vue";
-import { createPinia } from "pinia";
-import App from "@/App.vue";
-import { routes } from "@/router";
-import { createRouter, createWebHistory } from "vue-router";
-import { createWallet, sdkRoutes } from "@yeying-community/yeying-wallet";
-import { initializeProviders } from "@/plugins/account";
-import ElementPlus from "element-plus";
-import zhCn from "element-plus/dist/locale/zh-cn.mjs";
+import '@/assets/style.css'
+import { t } from '@yeying-community/yeying-i18n'
+import '@yeying-community/yeying-wallet/dist/yeying-wallet.css'
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from '@/App.vue'
+import { routes } from '@/router'
+import { createRouter, createWebHistory } from 'vue-router'
+import { createWallet, sdkRoutes } from '@yeying-community/yeying-wallet'
+import { initializeProviders } from '@/plugins/account'
+import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 
 const app = createApp(App)
 
-app.use(createPinia());
+app.use(createPinia())
 app.use(ElementPlus, {
-  locale: zhCn,
-});
+    locale: zhCn
+})
 
 app.config.globalProperties.$t = t
 
